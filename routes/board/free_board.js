dad6238843ea7a1064e89be14082037d8a1078b8
sun980120 <70737532+sun980120@users.noreply.github.com>
2021-03-22 14:01:34 +0900

    Modify Board

---
+++ routes/board/free_board.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var db = require("../../config/db");
+var router = express.Router();
+
+router.get('/write', function(req,res,next){
+    res.render('write/free_write',{user_id: req.session.user_id});
+});
+router.get('/:page', function(req, res, next) {
+    var page = req.params.page;
+    var sql_db = "select uid, title, name, date_format(regdate,'%Y-%m-%d') regdate from freeBoard";
+    db.query(sql_db,function(err,rows){
+        if(err) console.error("err : " + err);
+        res.render('board/board_Free',{title: '자유게시판', user_id:req.session.user_id,rows:rows,page:page,length:rows.length-1,page_num:5,pass:true});
+        console.log(rows.length-1);
+    })
+});
+router.post('/write', function(req,res){
+    var title = req.body.title;
+    var name = req.body.name;
+    var content = req.body.content;
+    var passwd = req.body.passwd;
+    var datas = [title,name,content,passwd];
+    var sql = "insert into freeBoard(title,name,content,regdate, passwd) values(?,?,?,now(),?)";
+    db.query(sql,datas,function(err,row){
+        if(err) console.error("err : " + err);
+        res.redirect('/free/1');
+    })
+});
+module.exports = router;
---
+++ routes/board/qna_board.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var db = require("../../config/db");
+var router = express.Router();
+
+
+router.get('/write', function(req,res,next){
+    res.render('write/qna_write',{user_id: req.session.user_id});
+});
+router.get('/:page', function(req, res, next) {
+    var page = req.params.page;
+    var sql_db = "select uid, title, name, date_format(regdate,'%Y-%m-%d') regdate from qnaBoard";
+    db.query(sql_db,function(err,rows){
+        if(err) console.error("err : " + err);
+        res.render('board/board_QnA',{title: 'QnA게시판', user_id:req.session.user_id,rows:rows,page:page,length:rows.length-1,page_num:5,pass:true});
+        console.log(rows.length-1);
+    })
+});
+router.post('/write', function(req,res){
+    var title = req.body.title;
+    var name = req.body.name;
+    var content = req.body.content;
+    var passwd = req.body.passwd;
+    var datas = [title,name,content,passwd];
+
+    var sql = "insert into qnaBoard(title,name,content,regdate, passwd) values(?,?,?,now(),?)";
+    db.query(sql,datas,function(err,row){
+        if(err) console.error("err : " + err);
+        res.redirect('/qna')
+    })
+});
+module.exports = router;
